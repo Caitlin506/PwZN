51cb1018692e2852087cb588861be13389d2cb1f
Katarzyna Stojek <62251578+Caitlin506@users.noreply.github.com>
2022-10-17 12:50:57 +0200

    Update histogram.py

--- lab1/histogram.py
+++ lab1/histogram.py
@@ -8,10 +8,13 @@ collections.Iterable = collections.abc.Iterable
 #from turtle import color
 #from ascii_graph import colors
 
-filename='ATaleOfTwoCities.txt'
-filename2='HrabiaMonteChristo.txt'
+parser = ap.ArgumentParser()
+parser.add_argument("file", help="Creates a histogram of words from a given file")
+args = parser.parse_args()
+
+filename=args.file
 histo=dict()
-histo2=dict()
+
 with open(filename, 'r') as f:
     for line in tqdm.tqdm(f,total=16285):
         sth=line.translate(str.maketrans('','',string.punctuation)).strip().split(' ')
@@ -20,27 +23,15 @@ with open(filename, 'r') as f:
                 histo[w] = histo[w]+1
             else:
                 histo[w]=1
-                
+try:
+    histo.pop('—')
+except:
+    pass
+
 histo.pop('')
+
 histogram = sorted(histo.items(), key=lambda x: x[1], reverse=True)
 
 graph = ag.Pyasciigraph()
 for line in graph.graph('Histogram dla pliku '+filename, histogram[:10]):
-    print(line)
-    
-with open(filename2, 'r') as f:
-    for line in tqdm.tqdm(f,total=34907):
-        sth=line.translate(str.maketrans('','',string.punctuation)).strip().split(' ')
-        for w in sth:
-            if w in histo2.keys():
-                histo2[w] = histo2[w]+1
-            else:
-                histo2[w]=1
-                
-histo2.pop('')
-histo2.pop('—')
-histogram2 = sorted(histo2.items(), key=lambda x: x[1], reverse=True)
-
-graph2 = ag.Pyasciigraph()
-for line in graph2.graph('Histogram dla pliku '+filename2, histogram2[:10]):
     print(line)
